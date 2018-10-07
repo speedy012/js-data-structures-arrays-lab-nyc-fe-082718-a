d0352fbeaff034dc93d1f8a71eae7889c084b6a3
Herman <hrivas5726@aol.com>
2018-10-07 18:59:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ function destructivelyAppendDriver(name){
 }
 
 //destructivelyRemovePrependDriver
-function destructivelyRemovePrependDriver(name) {
+function destructivelyPrependDriver(name) {
   drivers.unshift("Bob");
 }
