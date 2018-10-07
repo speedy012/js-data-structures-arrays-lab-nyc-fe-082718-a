bc48303c9432ed2a677722df374c81a71eee512c
Herman <hrivas5726@aol.com>
2018-10-07 20:45:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,6 +35,6 @@ function prependDriver(name) {
 
 //function removeLastDriver()
 function removeLastDriver() {
-  const copyofdrivers = [drivers-2]
-  return drivers;
+  const copyofdrivers = [drivers - 2];
+  return copyofdrivers;
 }
